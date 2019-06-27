b3c9b5a8b547fa2cb08a4145767567954738a89d
Andrés Valencia Téllez <ext-andres.valencia-tellez@here.com>
2019-06-27 09:32:59 +0200

    r106
    Signed-off-by: Andrés Valencia Téllez <ext-andres.valencia-tellez@here.com>

--- src/normal_frag.glsl
+++ src/normal_frag.glsl
@@ -92,7 +92,30 @@ float perspectiveDepthToViewZ( const in float invClipZ, const in float near, con
 	uniform float logDepthBufFC;
 	varying float vFragDepth;
 #endif
+#if NUM_CLIPPING_PLANES > 0
+	#if ! defined( PHYSICAL ) && ! defined( PHONG ) && ! defined( MATCAP )
+		varying vec3 vViewPosition;
+	#endif
+	uniform vec4 clippingPlanes[ NUM_CLIPPING_PLANES ];
+#endif
 void main() {
+	#if NUM_CLIPPING_PLANES > 0
+	vec4 plane;
+	#pragma unroll_loop
+	for ( int i = 0; i < UNION_CLIPPING_PLANES; i ++ ) {
+		plane = clippingPlanes[ i ];
+		if ( dot( vViewPosition, plane.xyz ) > plane.w ) discard;
+	}
+	#if UNION_CLIPPING_PLANES < NUM_CLIPPING_PLANES
+		bool clipped = true;
+		#pragma unroll_loop
+		for ( int i = UNION_CLIPPING_PLANES; i < NUM_CLIPPING_PLANES; i ++ ) {
+			plane = clippingPlanes[ i ];
+			clipped = ( dot( vViewPosition, plane.xyz ) > plane.w ) && clipped;
+		}
+		if ( clipped ) discard;
+	#endif
+#endif
 	#if defined( USE_LOGDEPTHBUF ) && defined( USE_LOGDEPTHBUF_EXT )
 	gl_FragDepthEXT = log2( vFragDepth ) * logDepthBufFC * 0.5;
 #endif
